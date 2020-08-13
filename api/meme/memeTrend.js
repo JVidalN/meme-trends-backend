4d825358f5da281eb67c2194eee97ed52c247020
JVidalN <j.vidalnunes@gmail.com>
2020-08-13 02:06:15 -0300

    Refactorining JS because frontend change

--- api/meme/memeTrend.js
+++ api/meme/memeTrend.js
@@ -5,7 +5,7 @@ const memeTrendSchema = new mongoose.Schema({
   name: { type: String, required: true },
   createdDate: { type: Date, min: '2010-01-01', max: '2100-12-31', required: true },
   trendDate: { type: Date, min: '2010-01-01', max: '2100-12-31', required: true },
-  vote: { type: String, required: true },
+  vote: { type: Number, required: true },
   meme: { type: String, required: true },
 });
 
